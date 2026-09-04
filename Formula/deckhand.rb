dc928e094ba945af77e85a1179779f85d9d478a4
FAUVIE Gilles (EQUANS BELUX) <gilles.fauvie+equans@external.equans.com>
2026-09-04 15:53:40 +0200

    deckhand 0.1.0

---
+++ Formula/deckhand.rb
@@ -0,0 +1,19 @@
+class Deckhand < Formula
+  desc "Turn a folder of HTML slides into a presentation: stage, phone remote, live audience"
+  homepage "https://deckhand.stranix.net"
+  url "https://github.com/stranix79/deckhand/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "22969a416ed8b25496295308777a552d83d7c820e8dab100e0d268b87708913a"
+  license "MIT"
+  head "https://github.com/stranix79/deckhand.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/stranix79/deckhand/internal/version.Version=#{version}"
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/deckhand"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/deckhand version")
+  end
+end
