1fb712a1549f63366d0ea20590cdf3eb62109037
FAUVIE Gilles (EQUANS BELUX) <gilles.fauvie+equans@external.equans.com>
2026-09-06 14:06:28 +0200

    deckhand 1.1.0

--- Formula/deckhand.rb
+++ Formula/deckhand.rb
@@ -1,8 +1,8 @@
 class Deckhand < Formula
   desc "Turn a folder of HTML slides into a presentation: stage, phone remote, live audience"
   homepage "https://deckhand.stranix.net"
-  url "https://github.com/stranix79/deckhand/archive/refs/tags/v1.0.5.tar.gz"
-  sha256 "a7bd492746d8bca8873287007b9d1a972cf8e9e62d496ffc2f1a135c90b8d281"
+  url "https://github.com/stranix79/deckhand/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "f59423621bc9597577fd7fbda090b32bfdd52a7cafdf7f8ea406ba21cad834e7"
   license "MIT"
   head "https://github.com/stranix79/deckhand.git", branch: "main"
 
